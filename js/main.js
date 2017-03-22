fe9c8148eee85ef0b2b22c3acd462b5e5199f43e
AMCorvi <amcorvi@icloud.com>
2017-03-22 19:40:42 -0400

    - Add basic transition for bar chart - Starting pie chart

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ let height = 400,
     width = 600,
     barWidth = 50,
     barOffset = 5;
+    radius = 200;
 
 // Linear scale allow data not to exceed maximum 
 let yScale = d3.scale.linear()
@@ -42,21 +43,83 @@ let colors = d3.scale.linear()
                 .domain( [ 0 , d3.max(barData)*.25, d3.max(barData)*.50, d3.max(barData)*.75, d3.max(barData) ])
                 .range(['#FFCB03', '#B222B2', '#2222B2', '#22B2B2', '#22B222'])
 
+// Adding tooltips
+let tooltip = d3.select('body').append('div')
+        .style('position', 'absolute')
+        .style('padding', '0 10px')
+        .style('background', 'white')
+        .style('opacity', 0)
 
 
-d3.select('#chart').append('svg')
+
+
+let barChart = d3.select('#chart').append('svg')
     .attr('width', width)
     .attr('height', height)
     .selectAll('rect').data(barData)
     .enter().append('rect')
         .style('fill', colors)
         .attr('width', xScale.rangeBand())
-        .attr('height', (d)=> {
-            return yScale(d);  
+        .attr('height', (data)=> {
+            return 0 ;  
         })
         .attr('x', (data, index)=> {
             return xScale(index) ; 
         })
         .attr('y', (data)=> {
+            return  height 
+        })
+    .on('mouseover', function(data){
+        tooltip.transition()
+            .style('opacity', .9)
+
+        tooltip.html(data)
+            .style('left', (d3.event.pageX - 35) + 'px')
+            .style('top',  (d3.event.pageY - 30) + 'px')
+
+        d3.select(this)
+            .style('opacity', '.3') 
+    })
+    .on('mouseout', function(data){
+        d3.select(this)
+            .style('opacity', '1') 
+    })
+    
+// Animate chart
+barChart.transition()
+    .attr('height', (data)=> {
+            return yScale(data);  
+        })
+    .attr('y', (data)=> {
             return  height - yScale(data);
         })
+    .delay((data, index)=>{
+        return data/2 * 20 ;
+    })
+    .duration(800)
+    .ease('elastic')
+
+
+toolTip = d3.select('body')
+                .style('position', 'absolute')
+                .style('padding', kk)
+
+
+// Pie Chart
+
+let pieChartData = [
+    {
+        label: "Bell",
+        value: 33
+    }
+    {
+        label: "Biv",
+        value: 33
+    }
+    {
+        label: "Devoe".
+        value: 33
+    }
+]
+
+
