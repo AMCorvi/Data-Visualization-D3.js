cc356d98f86b921518815e0a5c7d32a9c300dc99
AMCorvi <amcorvi@icloud.com>
2017-07-12 20:04:27 -0400

    Restructuring in order to begin rewrite 💀

--- js/main.js
+++ js/main.js
@@ -1,238 +1,250 @@
-/* ******   Data Value and Data Creation functions ***** */
+var bardata = [];
 
+for (var i=0; i < 50; i++) {
+    bardata.push(Math.round(Math.random()*100)+10)
+}
 
-// Create Array for bar chart data
-let barData = Array;
+bardata.sort(function compareNumbers(a,b) {
+    return a -b;
+});
 
-// Define function to create randomized amount of data
-let createDataPoints = (amount, max)=>{
+var margin = { top: 30, right: 30, bottom: 40, left:50 }
 
-    // Determine whether datapoint is greater then max and 
-    //   and decrease if  that is the case. 
-    if (max > 100) { max = max * .80; 
-        return max;
-    };
-
-    barData = Array.apply(null, new Array(amount) ) 
-    barData = barData.map(el => {
-      return   Math.floor( Math.random()*100 )       
-    });
-
-
-    return barData;
-};
-
-createDataPoints(Math.floor(Math.random()*100+5),82);
-// Remove
-console.log(barData)
-
-// Pie chart datapoints
-
-let pieChartData = [
-    {
-        label: "Bell",
-        value: 33
-    },
-    {
-        label: "Biv",
-        value: 33
-    },
-    {
-        label: "Devoe",
-        value: 33
-    },
-]
-// That girl is ...... ok now back to work!!!! 
-
-
-// Data display specifications
-//
-let height = 400,
-    width = 400,
+var height = 400 - margin.top - margin.bottom,
+    width = 600 - margin.left - margin.right,
     barWidth = 50,
-    barOffset = 5,
-    nodeWidth = 5,
-    radius = 200;
+    barOffset = 5;
 
-// Linear scale allow data not to exceed maximum 
-let yScale = d3.scale.linear()
-                .domain([0, d3.max(barData)])
-                .range([0, height])
+var tempColor;
 
-// Distribute all data across the x-axis evenly
-let xScale = d3.scale.ordinal()
-                .domain( d3.range(0, barData.length) )
-                .rangeBands([ 0, width ])
+var colors = d3.scale.linear()
+.domain([0, bardata.length*.33, bardata.length*.66, bardata.length])
+.range(['#B58929','#C61C6F', '#268BD2', '#85992C'])
 
-// Assign color of data-point relative to max and min
-let colors = d3.scale.linear()
-                .domain( [ 0 , d3.max(barData)*.25, d3.max(barData)*.50, d3.max(barData)*.75, d3.max(barData) ])
-                .range(['#FFCB03', '#B222B2', '#2222B2', '#22B2B2', '#22B222'])
+var yScale = d3.scale.linear()
+        .domain([0, d3.max(bardata)])
+        .range([0, height]);
 
+var xScale = d3.scale.ordinal()
+        .domain(d3.range(0, bardata.length))
+        .rangeBands([0, width], 0.2)
 
-// Colors for pie chart
-let pieColors = d3.scale.category10();
-
-// Adding tooltips
-let tooltip = d3.select('body').append('div')
+var tooltip = d3.select('body').append('div')
         .style('position', 'absolute')
         .style('padding', '0 10px')
         .style('background', 'white')
         .style('opacity', 0)
 
- /* ***** Visualization ***** */
-
-let barChart = d3.select('#chart').append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .selectAll('rect').data(barData)
+var barChart = d3.select('#chart').append('svg')
+    .style('background', '#E7E0CB')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate('+ margin.left +', '+ margin.top +')')
+    .selectAll('rect').data(bardata)
     .enter().append('rect')
-        .style('fill', colors)
+        .style('fill', function(d,i) {
+            return colors(i);
+        })
         .attr('width', xScale.rangeBand())
-        .attr('height', (data)=> {
-            return 0 ;  
+        .attr('x', function(d,i) {
+            return xScale(i);
         })
-        .attr('x', (data, index)=> {
-            return xScale(index) ; 
-        })
-        .attr('y', (data)=> {
-            return  height 
-        })
-    .on('mouseover', function(data){
+        .attr('height', 0)
+        .attr('y', height)
+
+    .on('mouseover', function(d) {
+
         tooltip.transition()
             .style('opacity', .9)
 
-        tooltip.html(data)
-            .style('color', 'firebrick')
+        tooltip.html(d)
             .style('left', (d3.event.pageX - 35) + 'px')
             .style('top',  (d3.event.pageY - 30) + 'px')
 
-        d3.select(this)
-            .style('opacity', '.3') 
-    })
-    .on('mouseout', function(data){
 
-        tooltip.html(null)
-
+        tempColor = this.style.fill;
         d3.select(this)
-            .style('opacity', '1') 
+            .style('opacity', .5)
+            .style('fill', 'yellow')
     })
-    
-// Animate chart
+
+    .on('mouseout', function(d) {
+        d3.select(this)
+            .style('opacity', 1)
+            .style('fill', tempColor)
+    })
+
 barChart.transition()
-    .attr('height', (data)=> {
-            return yScale(data);  
-        })
-    .attr('y', (data)=> {
-            return  height - yScale(data);
-        })
-    .delay((data, index)=>{
-        return index * 20 ;
+    .attr('height', function(d) {
+        return yScale(d);
     })
-    .duration(800)
+    .attr('y', function(d) {
+        return height - yScale(d);
+    })
+    .delay(function(d, i) {
+        return i * 20;
+    })
+    .duration(1000)
     .ease('elastic')
 
+var vGuideScale = d3.scale.linear()
+    .domain([0, d3.max(bardata)])
+    .range([height, 0])
 
-/* ****** Pie Chart ***** */
+var vAxis = d3.svg.axis()
+    .scale(vGuideScale)
+    .orient('left')
+    .ticks(10)
 
-let pie = d3.layout.pie()
-            .value((data)=>{
-                return data.value;
-            })
+var vGuide = d3.select('svg').append('g')
+    vAxis(vGuide)
+    vGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+    vGuide.selectAll('path')
+        .style({ fill: 'none', stroke: "#000"})
+    vGuide.selectAll('line')
+        .style({ stroke: "#000"})
 
-let arc = d3.svg.arc()
-            .outerRadius(radius)
+var hAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient('bottom')
+    .tickValues(xScale.domain().filter(function(d, i) {
+        return !(i % (bardata.length/5));
+    }))
 
-let pieChart = d3.select('#pieChart').append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-                .attr('transform', `translate(${width - radius}, ${height - radius})`)
-                .selectAll('path').data(pie(pieChartData))
-                .enter().append('g')
-                    .attr('class' , 'slice')
-
-let slices = d3.selectAll('g.slice')
-            .append('path')
-            .attr('fill', (data, index)=>{
-                    return pieColors(index)
-                })
-            .attr('d', arc)
-
-let text  = d3.selectAll('g.slice')
-            .append('text')
-            .text(function(data,index){
-                    return data.data.label
-                 })
-            .attr('text-anchor', 'middle')
-            .attr('transform', (data)=>{
-                data.innerRadius = 0;
-                data.outerRadius = radius;
-                return `translate(${arc.centroid(data)})`
-            })
+var hGuide = d3.select('svg').append('g')
+    hAxis(hGuide)
+    hGuide.attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
+    hGuide.selectAll('path')
+        .style({ fill: 'none', stroke: "#000"})
+    hGuide.selectAll('line')
+        .style({ stroke: "#000"})
 
 
 
 
-/* ***** Force Node Visualization ***** */
-let nodes = [
-        { name: "Bacon"},
-        { name: "Person 1", target:[0] },
-        { name: "Person 2", target:[1] },
-        { name: "Person 3", target:[2] },
-        { name: "Person 4", target:[0, 1, 2, 3] },
-        { name: "Person 5", target:[3] }
-]
 
-let links = [];
 
-for (var i = 0; i<nodes.length; i++){
-    if (nodes[i].target !==undefined){
-        for (var x = 0; x<nodes[i].target.length; x++){
-            links.push(
-                {
-                source: node[i],
-                target: nodes[i].target[x]
-                }
-            )
-        }
-    }
+
+
+
+
+
+
+
+var   w = 400,
+      h = 400;
+
+var circleWidth = 5;
+
+var palette = {
+      "lightgray": "#819090",
+      "gray": "#708284",
+      "mediumgray": "#536870",
+      "darkgray": "#475B62",
+
+      "darkblue": "#0A2933",
+      "darkerblue": "#042029",
+
+      "paleryellow": "#FCF4DC",
+      "paleyellow": "#EAE3CB",
+      "yellow": "#A57706",
+      "orange": "#BD3613",
+      "red": "#D11C24",
+      "pink": "#C61C6F",
+      "purple": "#595AB7",
+      "blue": "#2176C7",
+      "green": "#259286",
+      "yellowgreen": "#738A05"
+  }
+
+var nodes = [
+      { name: "Parent"},
+      { name: "child1"},
+      { name: "child2", target: [0]},
+      { name: "child3", target: [0]},
+      { name: "child4", target: [1]},
+      { name: "child5", target: [0, 1, 2, 3]}
+];
+
+var links = [];
+
+for (var i = 0; i< nodes.length; i++) {
+      if (nodes[i].target !== undefined) {
+            for (var x = 0; x< nodes[i].target.length; x++ ) {
+                  links.push({
+                        source: nodes[i],
+                        target: nodes[nodes[i].target[x]]
+                  })
+            }
+      }
 }
 
+var nodeChart = d3.select('#nodeChart')
+		.append('svg')
+		.attr('width', w)
+		.attr('height', h)
 
-let nodeChart = d3.select('#nodeChart')
-            .append('svg')
-            .attr('width' , width)
-            .attr('height', height)
+var force = d3.layout.force()
+	.nodes(nodes)
+	.links([])
+	.gravity(0.3)
+	.charge(-1000)
+	.size([w, h])
 
-let force = d3.layout.force()
-        .nodes(nodes)
-        .links([])
-        .charge(-1000)
-        .size(width, height)
+var link = nodeChart.selectAll('line')
+	.data(links).enter().append('line')
+	.attr('stroke', palette.gray)
 
-let link = nodeChart.selectAll('line')
-        .data(links).enter().append('line')
-        .attr('stroke', 'white')
-
-let node = nodeChart.selectAll('circle')
-        .data(nodes).enter()
-        .append('g')
-        .call(force.drag);
+var node = nodeChart.selectAll('circle')
+	.data(nodes).enter()
+	.append('g')
+	.call(force.drag);
 
 node.append('circle')
-    .attr('cx', (d)=>{
-        return d.x;
-    })
-    .attr('cy', (d)=>{
-        return d.y;
-    })
-    .attr('r', nodeWidth )
-    
-force.on('tick', (event)=>{
-    node.attr('transform', (data, index)=>{
-        return `translate(${d.x}, ${d.y})` ; 
-    })
+	.attr('cx', function(d) { return d.x; })
+	.attr('cy', function(d) { return d.y; })
+	.attr('r', circleWidth )
+	.attr('fill', function(d, i) {
+		if (i>0) { return palette.pink }
+		else { return palette.blue }
+	})
+
+node.append('text')
+	.text(function(d) { return d.name})
+	.attr('font-family', 'Roboto Slab')
+	.attr('fill', function(d, i) {
+		if (i>0) { return palette.mediumgray}
+		else { return palette.yellowgreen}
+	})
+	.attr('x', function(d, i) {
+		if (i>0) { return circleWidth + 4 }
+		else { return circleWidth -15 }
+	})
+	.attr('y', function(d, i) {
+		if (i>0) { return circleWidth }
+		else { return 8 }
+	})
+	.attr('text-anchor', function(d, i) {
+		if (i>0) { return 'beginning' }
+		else { return 'end'}
+	})
+	.attr('font-size',  function(d, i) {
+		if (i>0) { return '1em' }
+		else { return '1.8em'}
+	})
+
+force.on('tick', function(e) {
+	node.attr('transform', function(d, i) {
+		return 'translate('+ d.x +', '+ d.y +')';
+	})
+
+	link
+		.attr('x1', function(d) { return d.source.x })
+		.attr('y1', function(d) { return d.source.y })
+		.attr('x2', function(d) { return d.target.x })
+		.attr('y2', function(d) { return d.target.y })
 })
 
-force.start()
+
+force.start();
